6087edfaa97928966b54378589b450b46953ac47
Guy Marushchenko <guym@allegro.ai>
2019-10-14 16:14:54 +0300

    enable CORS

--- kamin_API.py
+++ kamin_API.py
@@ -1,8 +1,10 @@
 from flask import Flask, abort, request, jsonify, json
+from flask_cors import CORS
 from Controllers import discussion_controller
 from Entities.comment import *
 
 app = Flask(__name__)
+CORS(app)
 
 
 @app.route('/getDiscussion/<int:discussion_id>', methods=['GET'])
