d4e3d32df11c13568b3810450b320852b09ee65c
gal21 <gal.lahiani@gmail.com>
2020-01-09 16:39:53 +0200

    add change for simulation alerts

--- Controllers/discussion_controller.py
+++ Controllers/discussion_controller.py
@@ -23,27 +23,50 @@ class DiscussionController:
         return discussion_tree
 
     def get_comment_recursive(self, comment_dict, comments):
-        if len(comment_dict["child_comments"]) is 0:
-            comment = CommentNode(id=comment_dict["_id"].binary.hex(), author=comment_dict["author"],
-                                  text=comment_dict["text"], parent_id=comment_dict["parentId"],
-                                  discussion_id=comment_dict["discussionId"], extra_data=comment_dict["extra_data"],
-                                  actions=comment_dict["actions"], labels=comment_dict["labels"],
-                                  depth=comment_dict["depth"], time_stamp=comment_dict["time_stamp"],
-                                  child_comments=[], is_alerted=comment_dict["isAlerted"], alert=comment_dict["alert"])
+        if "isAlerted" in comment_dict:
+            if len(comment_dict["child_comments"]) is 0:
+                comment = CommentNode(id=comment_dict["_id"].binary.hex(), author=comment_dict["author"],
+                                      text=comment_dict["text"], parent_id=comment_dict["parentId"],
+                                      discussion_id=comment_dict["discussionId"], extra_data=comment_dict["extra_data"],
+                                      actions=comment_dict["actions"], labels=comment_dict["labels"],
+                                      depth=comment_dict["depth"], time_stamp=comment_dict["time_stamp"],
+                                      child_comments=[], is_alerted=comment_dict["isAlerted"])
+                return comment
+
+            child_list = []
+            for comment_id in comment_dict["child_comments"]:
+                child_comment_dict = comments[comment_id]
+                child_list.append(self.get_comment_recursive(child_comment_dict, comments))
+
+            comment = CommentNode(id=comment_dict["_id"].binary.hex(), author=comment_dict["author"], text=comment_dict["text"],
+                                  parent_id=comment_dict["parentId"], discussion_id=comment_dict["discussionId"],
+                                  extra_data=comment_dict["extra_data"], actions=comment_dict["actions"],
+                                  labels=comment_dict["labels"], depth=comment_dict["depth"],
+                                  time_stamp=comment_dict["time_stamp"], child_comments=child_list,
+                                  is_alerted=comment_dict["isAlerted"])
             return comment
+        else:
+            if len(comment_dict["child_comments"]) is 0:
+                comment = CommentNode(id=comment_dict["_id"].binary.hex(), author=comment_dict["author"],
+                                      text=comment_dict["text"], parent_id=comment_dict["parentId"],
+                                      discussion_id=comment_dict["discussionId"], extra_data=comment_dict["extra_data"],
+                                      actions=comment_dict["actions"], labels=comment_dict["labels"],
+                                      depth=comment_dict["depth"], time_stamp=comment_dict["time_stamp"],
+                                      child_comments=[])
+                return comment
 
-        child_list = []
-        for comment_id in comment_dict["child_comments"]:
-            child_comment_dict = comments[comment_id]
-            child_list.append(self.get_comment_recursive(child_comment_dict, comments))
+            child_list = []
+            for comment_id in comment_dict["child_comments"]:
+                child_comment_dict = comments[comment_id]
+                child_list.append(self.get_comment_recursive(child_comment_dict, comments))
 
-        comment = CommentNode(id=comment_dict["_id"].binary.hex(), author=comment_dict["author"], text=comment_dict["text"],
-                              parent_id=comment_dict["parentId"], discussion_id=comment_dict["discussionId"],
-                              extra_data=comment_dict["extra_data"], actions=comment_dict["actions"],
-                              labels=comment_dict["labels"], depth=comment_dict["depth"],
-                              time_stamp=comment_dict["time_stamp"], child_comments=child_list,
-                              is_alerted=comment_dict["isAlerted"], alert=comment_dict["alert"])
-        return comment
+            comment = CommentNode(id=comment_dict["_id"].binary.hex(), author=comment_dict["author"],
+                                  text=comment_dict["text"],
+                                  parent_id=comment_dict["parentId"], discussion_id=comment_dict["discussionId"],
+                                  extra_data=comment_dict["extra_data"], actions=comment_dict["actions"],
+                                  labels=comment_dict["labels"], depth=comment_dict["depth"],
+                                  time_stamp=comment_dict["time_stamp"], child_comments=child_list)
+            return comment
 
     def add_comment(self, comment_dict):
         comment = CommentNode(author=comment_dict["author"], text=comment_dict["text"],
