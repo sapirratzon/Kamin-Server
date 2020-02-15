100003af3de07f088f1adaf5fb4ff1d6c2f5030b
ronelhar <ronelharrar@gmail.com>
2020-02-15 19:33:35 +0200

    deleted unnecessary methods

--- SNA/data_processing.py
+++ SNA/data_processing.py
@@ -6,59 +6,6 @@ import pandas as pd
 
 from Entities.comment import CommentNode
 
-
-def seperated_networks_to_csv(file_path, path_to_save, ignore_deleted=True):
-    trees = tt.load_list_of_trees("80919_labeled_trees.txt")
-    # tt.extract_networks_from_trees(trees, ['DA', 'QU', 'MN'], 'networks.csv' )
-    first_tree = trees[0]
-    root_comment = CommentNode(author=first_tree['node']['author'], text=first_tree['node']['text'], parent_id=None,
-                               discussion_id=first_tree, extra_data=first_tree['node']['extra_data'],
-                               labels=first_tree['node']['labels'] if 'labels' in first_tree['node'] else None,
-                               depth=0, time_stamp=datetime.fromtimestamp(first_tree['node']['timestamp']),
-                               child_comments=[])
-
-    for discussion_id in range(0, 46):
-        tree = trees[discussion_id]
-        root_comment = CommentNode(author=tree['node']['author'], text=tree['node']['text'], parent_id=None,
-                                   discussion_id=tree, extra_data=tree['node']['extra_data'],
-                                   labels=tree['node']['labels'] if 'labels' in tree['node'] else None,
-                                   depth=0, time_stamp=datetime.fromtimestamp(tree['node']['timestamp']),
-                                   child_comments=[])
-        matrix = tt.answers_matrix(tree)
-        weighted_edge_list = tt.translate_matrix_to_edgelist(matrix['tree_map'], True)
-        final_edge_list = []
-        for edge in weighted_edge_list:
-            if '[deleted]' not in edge or not ignore_deleted:
-                final_edge_list.append(edge)
-        df = pd.DataFrame(final_edge_list, columns=['Source', 'Target', 'Weight'])
-        df.to_csv(path_to_save + "\\discussion_" + str(discussion_id) + "_weighted_edges.csv")
-
-
-def whole_network_to_csv(file_path, ignore_deleted=True):
-    matrix_list = []
-    trees = tt.load_list_of_trees("80919_labeled_trees.txt")
-    for i in range(0, 46):
-        tree = trees[i]
-        matrix_list.append(tt.answers_matrix(tree)['tree_map'])
-
-    weighted_edge_list = tt.translate_matrix_list_to_weighted_edge_list(matrix_list, ignore_deleted)
-    df = pd.DataFrame(weighted_edge_list, columns=['Source', 'Target', 'Weight'])
-    df.to_csv("DataSet\\all_weighted_edges.csv")
-
-
-"""
-source - author of reply
-target - target of reply
-weight - count of same reply 
-timestamp
-text - content 
-discussion idx = discussion idx in our title links file
-title - title of discussion
-ups
-downs
-"""
-
-
 def get_edges_csv(ignore_deleted=True):
     trees = tt.load_list_of_trees("80919_labeled_trees.txt")
     edges = {}
